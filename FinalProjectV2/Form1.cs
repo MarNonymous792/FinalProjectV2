a0184344aaef712f1540321b587b8d1b73af7073
MarNonymous792 <dacua.marceugene@gmail.com>
2026-05-08 15:21:40 +0800

    created a scholarship class
    Add Scholarship class and include in project file
    
    Added Scholarship.cs defining an internal Scholarship class with properties for scholarship_id, scholarship_name, description, and requirements. Updated FinalProjectV2.csproj to include the new source file.

--- FinalProjectV2/Form1.cs
+++ FinalProjectV2/Form1.cs
@@ -16,5 +16,35 @@ namespace FinalProjectV2
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
